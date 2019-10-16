78ab45e363fb7cfe91b43e7975eaec7b4b8cbf4a
Andrius Kairiukstis <k@andrius.mobi>
2019-10-16 07:27:52 +0200

    Better support of ARI WebSocket messages (events):
    - Chaining: it is possible to subsribe to the same event by different consumers
    - Removal: handlers could be removed (i.e. after playback is finished or call get terminated, user app is responsible to remove them);
    - event_filters: it does compare received ARI event with JSON template and processes only matched ones
    
    - Overall code update for ARI
    
    - Started working on examples and documentation

---
+++ spec/ari_event_handler_spec.cr
@@ -0,0 +1,167 @@
+require "./spec_helper"
+
+module Asterisk
+  class ARI
+    # TODO: open `def process_ws_message(json_data)`
+
+    # getter for ARI handlers
+    def handlers
+      @handlers
+    end
+  end
+end
+
+describe Asterisk::ARI do
+  ##############################################################################
+  # - generate a call and grab it by Stasis app
+  # - set custom event handler (on channel state change)
+  # - answer call, so its state will get changed
+  # - remove handler, terminate calls and ari
+  it "should set custom events handler and process events" do
+    with_ari do |ami, ari|
+      channel_id_ch = Channel(String).new
+      answer_ch     = Channel(Bool).new
+
+      ari.on_stasis_start do |event|
+        channel_id_ch.send event.channel.id
+      end
+
+      generate_call
+
+      # prevent chanels freezing
+      spawn do
+        sleep 0.5.seconds
+        channel_id_ch.send "failure"
+        answer_ch.send     false
+      end
+
+      # Wait until call appear in stasis
+      channel_id = channel_id_ch.receive
+      channel_id.should_not be "failure"
+
+      # Set custom event handler for event "ChannelStateChange" and Channel's channel_id
+      event_filter = JSON.parse(%({"type": "ChannelStateChange",
+                                   "channel": {"id": "#{channel_id}"}}))
+
+      handler_id = ari.on event_filter: event_filter do |event_json|
+        event = Asterisk::ARI::Events::ChannelStateChange.from_json(event_json)
+        if event.channel.state ==  "Up"
+          answer_ch.send true
+        end
+      end
+
+      # Get call answered, so custom handler will be executed in a while
+      ari.channels.answer channel_id: channel_id
+
+      # Wait until call state get changed (answered, expected to be "Up")
+      answer_ch.receive.should be_true
+
+      ari.remove_handler(handler_id)
+    end
+  end
+
+
+  ##############################################################################
+  # Often children methods or subclasses shall process event handlers for their
+  # ID. For example, `on_stasis_start` could be used in a generic way, but also with
+  # custom JSON filter on app_args (accuming that app_args is a JSON);
+  # others could be used with instance filter: (i.e. channel handlers with filter on
+  # `channel_id`; audio handlers with filter for `playback_id` or either both
+  # `playback_id`/`channel_id` (or `bridge_id`).
+  # That makes a need to support chained event handlers with option to remove
+  # them.
+  #
+  # - generate a call and grab it by Stasis app
+  # - set few event handlers
+  # - answer call, so its state will get changed
+  # - remove handler, terminate calls and ari
+  it "should process all the event handlers in chain" do
+    with_ari do |ami, ari|
+      channel_id_ch = Channel(String).new
+      handler_1     = Channel(Bool).new
+      handler_2     = Channel(Bool).new
+      handler_3     = Channel(Bool).new
+      handler_4     = Channel(Bool).new
+      handler_5     = Channel(Bool).new
+
+      handlers = Array(String).new
+
+      ari.on_stasis_start do |event|
+        channel_id_ch.send event.channel.id
+      end
+
+      # prevent chanels freezing
+      spawn do
+        sleep 0.5.seconds
+        channel_id_ch.send "failure"
+      end
+
+      generate_call
+
+      # Wait until call appear in stasis
+      channel_id = channel_id_ch.receive
+      channel_id.should_not be "failure"
+
+      # Set custom event handler for event "ChannelStateChange" and channel
+      # channel_id
+      event_filter = JSON.parse(%({"type": "ChannelStateChange",
+                                   "channel": {"id": "#{channel_id}"}}))
+
+      h = ari.on_channel_state_change do |event|
+        handler_1.send true if event.channel.state ==  "Up"
+      end
+      handlers.push h
+
+      h = ari.on_channel_state_change do |event|
+        handler_2.send true if event.channel.state ==  "Up"
+      end
+      handlers.push h
+
+      h = ari.on_channel_state_change do |event|
+        handler_3.send true if event.channel.state ==  "Up"
+      end
+      handlers.push h
+
+      # last two also has a filter on channel_id
+      h = ari.on_channel_state_change event_filter: JSON.parse(%({"channel": {"id": "#{channel_id}"}})) do |event|
+        handler_4.send true if event.channel.state ==  "Up"
+      end
+      handlers.push h
+
+      h = ari.on_channel_state_change event_filter: JSON.parse(%({"channel": {"id": "#{channel_id}"}})) do |event|
+        handler_5.send true if event.channel.state ==  "Up"
+      end
+      handlers.push h
+
+      # 1x on_stasis_start and 5x on_channel_state_change
+      ari.handlers.size.should eq 6
+
+      # Get call answered, so custom handler will be executed in a while
+      ari.channels.answer channel_id: channel_id
+
+      # prevent chanels freezing
+      spawn do
+        sleep 0.5.seconds
+        handler_1.send false
+        handler_2.send false
+        handler_3.send false
+        handler_4.send false
+        handler_5.send false
+      end
+
+      # Wait until call state get changed (answered, expected to be "Up")
+      handler_1.receive.should be_true
+      handler_2.receive.should be_true
+      handler_3.receive.should be_true
+      handler_4.receive.should be_true
+      handler_5.receive.should be_true
+
+      handlers.each do |handler_id|
+        ari.remove_handler(handler_id)
+      end
+
+      # After remove, only one handler keep remaining (on_stasis_start)
+      ari.handlers.size.should eq 1
+    end
+  end
+end
--- src/asterisk/ari.cr
+++ src/asterisk/ari.cr
@@ -35,8 +35,11 @@ module Asterisk
 
     private getter! ws : HTTP::WebSocket?
 
+    # HTTP client connection lock
     @conn_lock = Mutex.new
+
     @client : HTTP::Client?
+
     def client
       @conn_lock.synchronize do
         @client.as(HTTP::Client)
@@ -53,6 +56,8 @@ module Asterisk
       @uri = URI.parse(@url)
     end
 
+    # Start ARI connections (HTTP client, WS-client and event handlers)
+    # TODO: client handlers for on_connected and on_close
     def start
       if connected?
         logger.info "#{self.class}: Already started"
@@ -66,22 +71,7 @@ module Asterisk
       end
 
       ws.on_message do |json_data|
-        event = JSON.parse(json_data)
-
-        # Basic validation of the ws message.
-        # ARI event is a hash-like structure with field `type` at the root,
-        # containing string (ARI event name)
-        unless event.as_h?.try &.["type"]?.try &.as_s?
-          logger.error "Message does not look like Asterisk ARI event:\n#{json_data}\n---"
-          next
-        end
-
-        # find registered for event handlers and execute them
-        handlers_for(event).each do |handler|
-          spawn do
-            handler.call(json_data)
-          end
-        end
+        process_ws_message json_data
       end
 
       spawn do
@@ -89,50 +79,99 @@ module Asterisk
       end
     end
 
+    private def connect
+      @client = HTTP::Client.new(@uri)
+      client.basic_auth(@username, @password)
+
+      # connect to the HTTP(s) interface and get asterisk version
+      info = JSON.parse(get("asterisk/info").body.to_s)
+      if info["message"]?.to_s =~ /Authentication required/i
+        raise AuthenticationError.new(info["message"].to_s)
+      end
+
+      # Execute connection to the Asterisk API and set
+      # `asterisk_version` and `entity_id`
+      @asterisk_version = info["system"]["version"].to_s
+      # mac-address
+      @entity_id = info["system"]["entity_id"].to_s
+
+      # Connect to the WS/WSS.
+      # ARI events will be received through on_message callback
+      query_params = HTTP::Params.encode({"api_key" => "#{@username}:#{@password}", "app" => @app}).to_s
+      @ws = HTTP::WebSocket.new("#{@url}/events?#{query_params}")
+      raise ConnectionError.new("Can't connect to the #{@url}") if ws.closed?
+
+      # TODO:
+      # Host is down, wrong port etc
+      # Error connecting to '127.0.0.1:8088': Connection refused (Errno)
+      # Wrong credentials
+      # Handshake got denied. Status code was 401. (Socket::Error)
+    end
+
+    # close ARI http and ws-connections
+    def close
+      client.close
+      ws.close unless closed?
+    end
+
+    def connected?
+      ws? && ! ws.closed?
+    end
+
+    def closed?
+      ws.nil? || (ws? && ws.closed?)
+    end
+
+    # Include ARI resources with ARI instance, so once ARI conenction get
+    # created, resources are available within it, i.e.:
+    # ```
+    # ari.channels.answer channel_id: event.channel.id
+    # ```
+    {% begin %}
+      {% for t in Resources.all_subclasses %}
+        {% klass = t.stringify.split("::").last.id %}
+        {% resource = klass.stringify.underscore.id %}
+        @{{resource}} : {{klass}}? = nil
+
+        def {{resource}}
+          @{{resource}} ||= {{klass}}.new(ari: self)
+        end
+      {% end %}
+    {% end %}
+
     # Map of ARI events as a hash (event_name => EventName)
     {% begin %}
-      {% events = {} of String => Class %}
-      {% Events.constants.map do |klass|
-        klass_name = klass.stringify
-        unless %w(Message Event).includes?(klass_name)
-          events[klass_name] = ("Events::" + klass.stringify).id
+      {%
+        events = {} of String => Class
+
+        Events.constants.map do |klass|
+          klass_name = klass.stringify
+          unless %w(Message Event).includes?(klass_name)
+            events[klass_name] = ("Events::" + klass.stringify).id
+          end
         end
-      end %}
+      %}
       @events_map = {{events}}
     {% end %}
 
-    # Generates events handlers for known ARI events.
-    # ```
-    # def on_device_state_changed(&@on_device_state_changed : Events::DeviceStateChanged ->)
-    # end
-    # ```
-    {% begin %}
-      {% for t in Events.constants %}
-        {% event = t.stringify %}
-        {% unless %w(Message Event).includes?(event) %}
-          {% klass = ("Events::" + event).id %}
-          {% method = event.underscore.id %}
-          {% event = event.id %}
+    private def process_ws_message(json_data)
+      event = JSON.parse(json_data)
 
-          def on_{{method}}(&block : {{klass}} ->)
-            event_filter = JSON.parse(%({"type": "{{event}}"}))
-            on(event_filter) do |json_data|
-              event_data = {{klass}}.from_json(json_data)
-              block.call(event_data)
-            end
-          end
+      # Basic validation of the ws message.
+      # ARI event is a hash-like structure with field `type` at the root,
+      # containing string (ARI event name)
+      unless event.as_h?.try &.["type"]?.try &.as_s?
+        logger.error "Message does not look like Asterisk ARI event:\n#{json_data}\n---"
+        return
+      end
 
-          def on_{{method}}(handler_id : String, &block : {{klass}} ->)
-            event_filter = JSON.parse(%({"type": "{{event}}"}))
-            on(handler_id, event_filter) do |json_data|
-              event_data = {{klass}}.from_json(json_data)
-              block.call(event_data)
-            end
-          end
-
-        {% end %}
-      {% end %}
-    {% end %}
+      # find registered for event handlers and execute them
+      handlers_for(event).each do |handler|
+        spawn do
+          handler.call(json_data)
+        end
+      end
+    end
 
     @handlers = Hash(String, Hash(JSON::Any, Proc(String, Nil))).new
 
@@ -159,7 +198,7 @@ module Asterisk
       }.flatten.compact
     end
 
-    # Match JSON::Any message against JSON::Any event_filter. event_filter should be
+    # Match JSON::Any against JSON::Any event_filter. event_filter should be
     # a Hash of Strings or Hash of Hashes of Strings.
     #
     # https://play.crystal-lang.org/#/r/7lam
@@ -185,56 +224,62 @@ module Asterisk
       !result.includes?(false)
     end
 
-    macro resources
-      {% for t in Resources.all_subclasses %}
-        {% klass = t.stringify.split("::").last.id %}
-        {% resource = klass.stringify.underscore.id %}
-        @{{resource}} : {{klass}}? = nil
-        def {{resource}}
-          @{{resource}} ||= {{klass}}.new(ari: self)
-        end
+    # Generates events handlers for known ARI events:
+    # ```
+    # def on_device_state_changed(event_filter : JSON::Any = JSON.parse("{}"), &block : Events::DeviceStateChanged ->)
+    #   # set filter on `type`
+    #   event_filter.as_h["type"] = JSON.parse(%("DeviceStateChanged"))
+    #   on(event_filter) do |json_data|
+    #     event_data = {{klass}}.from_json(json_data)
+    #     block.call(event_data)
+    #   end
+    # end
+    # ```
+    #
+    # and exactly same but with user-created `handler_id`:
+    # ```
+    # def on_device_state_changed(handler_id : String, event_filter : JSON::Any = JSON.parse("{}"), &block : Events::DeviceStateChanged ->)
+    #   # ...
+    # end
+    # ```
+    #
+    # Usage example:
+    # ```
+    # handler_id = ari.on_stasis_start do |event|
+    #   ari.channels.answer channel_id: event.channel.id
+    #   # ...
+    # end
+    # # ...
+    # ari.remove_handler(handler_id)
+    # ```
+    {% begin %}
+      {% for t in Events.constants %}
+        {% event = t.stringify %}
+        {% unless %w(Message Event).includes?(event) %}
+          {% klass = ("Events::" + event).id %}
+          {% method = event.underscore.id %}
+          {% event = event.id %}
+
+          def on_{{method}}(event_filter : JSON::Any = JSON.parse("{}"), &block : {{klass}} ->)
+           # set filter on `type`
+            event_filter.as_h["type"] = JSON.parse(%("{{event}}"))
+            on(event_filter) do |json_data|
+              event_data = {{klass}}.from_json(json_data)
+              block.call(event_data)
+            end
+          end
+
+          def on_{{method}}(handler_id : String, event_filter : JSON::Any = JSON.parse("{}"), &block : {{klass}} ->)
+            # set filter on `type`
+            event_filter.as_h["type"] = JSON.parse(%("{{event}}"))
+            on(handler_id, event_filter) do |json_data|
+              event_data = {{klass}}.from_json(json_data)
+              block.call(event_data)
+            end
+          end
+
+        {% end %}
       {% end %}
-      end
-    resources
-
-    private def connect
-      @client = HTTP::Client.new(@uri)
-      client.basic_auth(@username, @password)
-
-      # connect to the HTTP(s) interface and get asterisk version
-      info = JSON.parse(get("asterisk/info").body.to_s)
-      if info["message"]?.to_s =~ /Authentication required/i
-        raise AuthenticationError.new(info["message"].to_s)
-      end
-
-      # Execute connection to the Asterisk API and set
-      # asterisk_version and entity_id
-      @asterisk_version = info["system"]["version"].to_s
-      @entity_id        = info["system"]["entity_id"].to_s # mac-address
-
-      # Connect to the WS/WSS.
-      # ARI events will be received through on_message callback
-      query_params = HTTP::Params.encode({"api_key" => "#{@username}:#{@password}", "app" => @app}).to_s
-      @ws = HTTP::WebSocket.new("#{@url}/events?#{query_params}")
-      raise ConnectionError.new("Can't connect to the #{@url}") if ws.closed?
-
-      # Host is down, wrong port etc
-      # Error connecting to '127.0.0.1:8088': Connection refused (Errno)
-      # Wrong credentials
-      # Handshake got denied. Status code was 401. (Socket::Error)
-    end
-
-    def close
-      client.close
-      ws.close unless closed?
-    end
-
-    def connected?
-      ws? && ! ws.closed?
-    end
-
-    def closed?
-      ws.nil? || (ws? && ws.closed?)
-    end
+    {% end %}
   end
 end
